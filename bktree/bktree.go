4e99307bb7412aa671a3af458483803467df3e54
Jean Elbers <jean.elbers@gmail.com>
2021-07-27 07:31:44 +0200

    Update bktree.go

--- bktree/bktree.go
+++ bktree/bktree.go
@@ -3,7 +3,7 @@ package bktree
 import (
         "fmt"
 
-        "github.com/agatan/bktree"
+	"github.com/agatan/bktree"
         levenshtein "github.com/creasty/go-levenshtein"
 )
 
@@ -18,11 +18,18 @@ func (x word) Distance(e bktree.Entry) int {
 }
 
 func Add() {
-        var tree bktree.BKTree
-        // add words
-        words := []string{"dog", "cat"}
-        for _, w := range words {
-                tree.Add(word(w))
-        }
-        return tree
+	var tree bktree.BKTree
+	// add words
+	words := []string{"AAAA", "AAAG", "AAAT", "AAAC", "AACC", "AACT", "AACG", "AAGC"}
+	for _, w := range words {
+		tree.Add(word(w))
+	}
+
+	// spell check
+        inputword := string("AAAA")
+	results := tree.Search(word(inputword), 2)
+	fmt.Printf("Input is %s. \nDid you mean:\n", inputword)
+	for _, result := range results {
+		fmt.Printf("\t%s (distance: %d)\n", result.Entry.(word), result.Distance)
+	}
 }
